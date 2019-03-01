d8f9082b8c34a5f88492379ea93b11ad404d6f3d
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-03-01 11:53:28 -0500

    React Router set up. Routes declared for Smurfs.js and SmurfForm.js. Used render prop to pass down required props.

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { Route } from "react-router-dom";
 
 import "./App.css";
 import SmurfForm from "./components/SmurfForm";
@@ -44,8 +45,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm submitSmurf={this.submitSmurf} />
-        <Smurfs smurfs={this.state.smurfs} />
+        <Route
+          exact
+          path="/"
+          render={routeProps => (
+            <Smurfs {...routeProps} smurfs={this.state.smurfs} />
+          )}
+        />
+        <Route
+          path="/smurf-form"
+          render={routeProps => (
+            <SmurfForm {...routeProps} submitSmurf={this.submitSmurf} />
+          )}
+        />
       </div>
     );
   }
